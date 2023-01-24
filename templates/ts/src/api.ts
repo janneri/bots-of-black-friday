7d3fd411ad7d4686984c73fb6a1d9faa1708a15d
Juho Leinonen <juho.leinonen@solita.fi>
2023-01-24 10:26:33 +0200

    Implement interface types for register api call response

--- templates/ts/src/api.ts
+++ templates/ts/src/api.ts
@@ -1,20 +1,31 @@
+import { Player } from "./types/Player";
+import { GameMap } from "./types/GameMap";
+
 const LOCAL_URL = "http://localhost:8080";
 const PROD_URL = "https://bots-of-black-friday.azurewebsites.net";
 
-const getUrl = (): String => {
+const getUrl = (): string => {
   if (process.env.NODE_ENV === "development") {
     return LOCAL_URL;
   }
   return PROD_URL;
 };
 
-export const register = (playerName: String) => {
+const headers = {
+  "content-type": "application/json;charset=UTF-8",
+};
+
+interface RegisterResponse {
+  id: string;
+  player: Player;
+  map: GameMap;
+}
+
+export const register = (playerName: string): Promise<RegisterResponse> => {
   console.log(`${getUrl()}/register`);
   return fetch(`${getUrl()}/register`, {
     method: "POST",
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-    },
+    headers: headers,
     body: JSON.stringify({ playerName: playerName }),
   }).then((response: Response) => response.json());
 };
