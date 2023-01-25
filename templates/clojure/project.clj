68941515de80297696afee68a56860d733bf41de
Juho Leinonen <juho.leinonen@solita.fi>
2023-01-25 16:17:41 +0200

    Implement Clojure template

---
+++ templates/clojure/project.clj
@@ -0,0 +1,12 @@
+(defproject clojure-bot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [cheshire "5.11.0"]
+                 [clj-http "3.12.3"]]
+  :main ^:skip-aot clojure-bot.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
