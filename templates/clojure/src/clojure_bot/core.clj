830996c0bdea3bb1a66bfbad195986b88b021238
Juho Leinonen <juho.leinonen@solita.fi>
2023-01-26 13:17:45 +0200

    Comment to Clojure template that the register response contains the game map and that you want to get the current game-state before a move

--- templates/clojure/src/clojure_bot/core.clj
+++ templates/clojure/src/clojure_bot/core.clj
@@ -4,7 +4,9 @@
 
 (defn -main
   [& args]
+  ;; game-info contains the game map
   (let [game-info (api/register "My cool Clojure bot")]
     (while true
       (Thread/sleep 1000)
+      ;; You probably want to get the current game-state from the server before you do your move
       (api/move (:id game-info) (rand-nth ["LEFT" "RIGHT"])))))
